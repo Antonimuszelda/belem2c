0257f931dcb6c61ea422bfbbea29aed883d689c6
antony davi <astroantonyy@gmail.com>
2025-10-25 15:18:08 -0300

    Corrigido hardcode localhost:8000 e trocado Stadia Maps por OSM

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,14 @@
-// frontend/vite.config.ts
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   
-  // =========================================================
-  // ✅ CORREÇÃO CRÍTICA: ADICIONE ESTA LINHA
+  
   base: './', 
-  // =========================================================
   
   optimizeDeps: {
-    // Apenas mantenha as dependências do Leaflet que você realmente usa
+  
     include: ['react-leaflet', 'leaflet', 'react-leaflet-draw', 'leaflet-draw'], 
   },
 });
