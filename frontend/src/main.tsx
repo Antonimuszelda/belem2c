ef9e33a826d786f0266617c7fe754bf2e08e545a
Antony <Antonimuszelda@users.noreply.github.com>
2025-12-05 00:34:12 -0300

    Remove Leaflet CSS import from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'leaflet/dist/leaflet.css';
 import '../public/fonts/icofont.css';
 
 const root = ReactDOM.createRoot(
